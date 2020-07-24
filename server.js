cd1352dbf557d39518e098d846b56ec5174f7f85
adler3d <adler3d@gmail.com>
2020-07-24 15:21:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const net = require('net');
+
 var inp=`12
 ####################
 #   #    #   #   #@#
@@ -12,4 +14,13 @@ var inp=`12
 #   #    #   #     #
 ####################
 {"url":"http://borgar.net/programs/sokoban/levels/Xsokoban.txt","level_id":4}`;
-process.stdout.write("inp");
+process.stdout.write(inp);
+
+var server=net.createServer(c=>{
+  console.log('client connected');
+  c.on('end',()=>{console.log('client disconnected');});
+  c.write(inp);
+  c.pipe(process.stdout);
+});
+server.on('error',err=>{throw err;});
+server.listen(30500,()=>{console.log('server bound');});
