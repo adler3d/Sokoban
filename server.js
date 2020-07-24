925cacd66c320908dc93fa84dfee7da85658a4a8
adler3d <adler3d@gmail.com>
2020-07-24 16:30:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const net = require('net');
-
-var inp=`12
+var lvl_x4=`12
 ####################
 #   #    #   #   #@#
 # $      $   $   # #
@@ -14,13 +13,37 @@ var inp=`12
 #   #    #   #     #
 ####################
 {"url":"http://borgar.net/programs/sokoban/levels/Xsokoban.txt","level_id":4}`;
+
+var lvl_1=`10
+    #####
+    #   #
+    #$  #
+  ###  $##
+  #  $ $ #
+### # ## #   ######
+#   # ## #####  ..#
+# $  $          ..#
+##### ### #@##  ..#
+    #     #########
+    #######
+{"url":"http://borgar.net/programs/sokoban/levels/Sokoban.txt","level_id":1}`
+
+var intro=`5
+#####
+#@  #
+# #$###
+# $ ..#
+#######
+{"url":"http://borgar.net/programs/sokoban/levels/Intro.txt","level_id":1}`;
+var inp=[intro,lvl_1,lvl_x4][process.argv.length==3?process.argv[3]|0:0];
 process.stdout.write(inp);
 
+var say=s=>process.stdout.write('\n'+s+'\n');
 var server=net.createServer(c=>{
-  console.log('client connected');
-  c.on('end',()=>{console.log('client disconnected');});
-  c.write(inp);
+  process.stdout.write('client connected');
+  c.on('end',()=>{say('client disconnected');process.exit();});
+  c.write(intro);
   c.pipe(process.stdout);
 });
 server.on('error',err=>{throw err;});
-server.listen(30500,()=>{console.log('server bound');});
+server.listen(30500,()=>{say('server bound');});
